ae69f0233729487aee6e561a190636241099a4c1
heberAO <heberadrian50@gmail.com>
2026-01-08 14:09:05 -0300

    Modify guardar_datos to include external work details
    Updated the guardar_datos function to include 'externo' parameter and adjusted QR code generation accordingly.

--- tarjetas.py
+++ tarjetas.py
@@ -83,7 +83,7 @@ if st.button("💾 GUARDAR REGISTRO Y GENERAR INFORME"):
     if not tag or not responsable:
         st.error("⚠️ Tag y Responsable son obligatorios.")
     else:
-        exito, msj = guardar_datos(fecha, responsable, tag, potencia, tension, corriente, rpm, res_tierra, res_bobinas, descripcion)
+        exito, msj = guardar_datos(fecha, responsable, tag, potencia, tension, corriente, rpm, res_tierra, res_bobinas, descripcion, externo)
         if exito:
             # Generar QR
             fecha_qr = fecha.strftime("%d/%m/%Y")
@@ -94,6 +94,7 @@ if st.button("💾 GUARDAR REGISTRO Y GENERAR INFORME"):
                f"R.TIERRA: {res_tierra}\n"
                f"R.BOBINAS: {res_bobinas}\n"
                f"DESC: {descripcion}"
+               f"EXTERNO {externo}"
             )
             qr = qrcode.make(qr_text)
             buf_qr = BytesIO()
@@ -150,6 +151,7 @@ if st.button("💾 GUARDAR REGISTRO Y GENERAR INFORME"):
             st.error(f"Error: {msj}")
             st.markdown("---")
 st.caption("Sistema diseñado y desarrollado por **Heber Ortiz** | Marpi Electricidad ⚡")
+
 
 
 
