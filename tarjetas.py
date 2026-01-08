21b8b4ee23b9d625f049fa8316d90d543f917c82
heberAO <heberadrian50@gmail.com>
2026-01-08 10:49:28 -0300

    Update tarjetas.py

--- tarjetas.py
+++ tarjetas.py
@@ -85,13 +85,13 @@ if st.button("💾 GUARDAR REGISTRO Y GENERAR INFORME"):
         exito, msj = guardar_datos(fecha, responsable, tag, potencia, tension, corriente, rpm, res_tierra, res_bobinas, descripcion)
     if exito:
             st.success(msj)
-        st.divider()
-        st.subheader("¿Terminaste con este motor?")
-        if st.button("🧹 LIMPIAR FORMULARIO PARA NUEVA CARGA"):
-           for key in list(st.session_state.keys()):
-                        if key.startswith("ins_"):
-                            st.session_state[key] = ""
-                    st.rerun() 
+            st.divider()
+            st.subheader("¿Terminaste con este motor?")
+            if st.button("🧹 LIMPIAR FORMULARIO PARA NUEVA CARGA"):
+               for key in list(st.session_state.keys()):
+                   if key.startswith("ins_"):
+                       st.session_state[key] = ""
+               st.rerun() 
         
             # Generar QR
             fecha_qr = fecha.strftime("%d/%m/%Y")
@@ -151,6 +151,7 @@ if st.button("💾 GUARDAR REGISTRO Y GENERAR INFORME"):
             st.error(f"Error: {msj}")
             st.markdown("---")
 st.caption("Sistema diseñado y desarrollado por **Heber Ortiz** | Marpi Electricidad ⚡")
+
 
 
 
