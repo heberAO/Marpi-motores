5b6e422410d473e15bd4dab8a128ee81273bf8cb
heberAO <heberadrian50@gmail.com>
2026-01-08 14:14:58 -0300

    Update tarjetas.py

--- tarjetas.py
+++ tarjetas.py
@@ -48,7 +48,7 @@ with col_m3:
     res_ineterna = st.text_input("Resistencia Interna (Ω)", help="V-V, U-U, W-W", key="ins_int")
 
 descripcion = st.text_area("Detalles de Reparación y Repuestos", key="ins_d")
-externo_tabajos = st.text_area("Reparacion Taller Externo", key="ins_externo")
+externo = st.text_area("Reparacion Taller Externo", key="ins_externo")
 
 # --- FUNCIÓN GUARDAR ---
 def guardar_datos(f, r, t, pot, ten, corr, vel, rt, rb, d, externo):
@@ -69,7 +69,7 @@ def guardar_datos(f, r, t, pot, ten, corr, vel, rt, rb, d, externo):
             "Res_Bobinas": rb,
             "Res_interna": int,
             "Descripcion": d,
-            "Externo_tabajos": externo,
+            "Externo": externo,
         }])
         
         df_final = pd.concat([df_existente, nuevo_registro], ignore_index=True)
@@ -151,6 +151,7 @@ if st.button("💾 GUARDAR REGISTRO Y GENERAR INFORME"):
             st.error(f"Error: {msj}")
             st.markdown("---")
 st.caption("Sistema diseñado y desarrollado por **Heber Ortiz** | Marpi Electricidad ⚡")
+
 
 
 
