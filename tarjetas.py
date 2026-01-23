259e6b235a68aa54c09d061fc91f385202d049c5
heberAO <heberadrian50@gmail.com>
2026-01-23 15:48:05 -0300

    Update tarjetas.py

--- tarjetas.py
+++ tarjetas.py
@@ -202,7 +202,7 @@ if modo == "Nuevo Registro":
         r_la, r_loa = r1.text_input("Rodamiento LA").upper(), r2.text_input("Rodamiento LOA").upper()
         # Dentro del formulario de Reparación
         tipo_rodamiento = st.selectbox(
-            "Tipo de rodamientos instalados:",["Abierto (Sin sellos)","RS Sello de un solo lado", "2RS (Sello de Goma - Hermético)", "ZZ (Blindaje Metálico)"])    
+            "Tipo de rodamientos instalados:",["Abierto (Sin sellos)","RS Sello de un solo lado", "2RS (Sello Caucho Sintetico - Hermético)", "ZZ (Blindaje Metálico)"])    
 
         st.subheader("⚡ Mediciones Eléctricas")
         m1, m2, m3 = st.columns(3)
@@ -693,6 +693,7 @@ elif modo == "Mediciones de Campo":
             
 st.markdown("---")
 st.caption("Sistema desarrollado y diseñado por Heber Ortiz | Marpi Electricidad ⚡")
+
 
 
 
